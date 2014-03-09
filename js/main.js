cb8e46bd0b916a83893953455253b6cebf906a00
Joseph Flasher <flasher@istrategylabs.com>
2014-03-09 15:13:03 -0400

    Small changes to profile styling

--- js/main.js
+++ js/main.js
@@ -18,7 +18,24 @@ var data = {
     ]
 };
 
-
 //Get the context of the canvas element we want to select
+try {
+  var myLine = new Chart(document.getElementById("myChart").getContext("2d")).Line(data);
+} catch(err) {}
 
-var myLine = new Chart(document.getElementById("myChart").getContext("2d")).Line(data);
+// For the profile chart
+var data2 = {
+    labels : ["January","February","March","April","May","June","July"],
+    datasets : [
+        {
+            fillColor : "rgba(151,187,205,0.5)",
+            strokeColor : "rgba(151,187,205,1)",
+            pointColor : "rgba(151,187,205,1)",
+            pointStrokeColor : "#fff",
+            data : [28,48,40,19,96,27,100]
+        }
+    ]
+};
+try {
+  var myLine2 = new Chart(document.getElementById("profileChart").getContext("2d")).Line(data2);
+} catch (err) {}
