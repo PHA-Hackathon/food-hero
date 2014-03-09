786a11d2df1c5512eda5110fae0f757c29b4b0ee
Philip Ashlock <phil@civicagency.org>
2014-03-08 22:39:47 -0500

    Cleaning up html

--- bootstrap/header.php
+++ bootstrap/header.php
@@ -36,13 +36,12 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#">Food Hero</a>
+          <a class="navbar-brand" href="/">Food Hero</a>
         </div>
         <div class="navbar-collapse collapse">
           <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#contact">Contact</a></li>
+            <li class="active"><a href="/">Dashboard</a></li>
+            <li><a href="profile.php">Profile</a></li>
             <li class="dropdown">
               <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
               <ul class="dropdown-menu">
